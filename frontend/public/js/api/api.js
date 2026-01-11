fa82a91f7b56827f13ad9ba6967346d8ad28de43
sizu-san <kamiku851@gmail.com>
2026-01-11 21:10:56 +0530

    backend url updated

--- frontend/public/js/api/api.js
+++ frontend/public/js/api/api.js
@@ -1,4 +1,4 @@
-const base_url = 'http://localhost:3000';
+const base_url = 'https://taskspark-backend.onrender.com';
 
 export async function api(endpoint , options={}){ 
 const token = localStorage.getItem('token');
